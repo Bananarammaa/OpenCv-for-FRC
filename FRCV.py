555c3ffa9182a7a8d459fbc4c8e04810e34a3011
sl3dman <Joseph@Josephs-MacBook-Pro.local>
2016-10-10 19:54:50 -0600

    NetworkTables&Boxes
    Got pyNetworkTables and the different bounding rectangles to function

--- FRCV.py
+++ FRCV.py
@@ -1,6 +1,7 @@
+import cv
 import cv2
 import numpy as np
-#from networktables import NetworkTable
+from networktables import NetworkTable
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -9,13 +10,13 @@ import os
 import sys
 import time
 # network table setup
-'''
+
 #commented out for testing
-#NetworkTable.setIPAddress("127.0.0.1")#127.0.0.1 with tester program
-#NetworkTable.setClientMode()
-#NetworkTable.initialize()
-#sd = NetworkTable.getTable("SmartDashboard")
-'''
+NetworkTable.setIPAddress("localhost")#127.0.0.1 with tester program
+NetworkTable.setClientMode()
+NetworkTable.initialize()
+sd = NetworkTable.getTable("SmartDashboard")
+
 # VideoCapture webcam id=0,1,2,3...
 vc = cv2.VideoCapture(0)
 
@@ -37,6 +38,7 @@ time_idx = 0
 time_start = time.time()
 camfps=0
 i = 0
+j = 0
 found = False
 #Loop to process video
 while rval:
@@ -77,10 +79,10 @@ while rval:
 
     mask = cv2.inRange(hsv, lower_teal, upper_teal)# for tower'''
     '''mask = cv2.inRange(hsv, lower_red, upper_red)#for testing'''
-    cv2.imshow("hsv", mask)
+    #cv2.imshow("hsv", mask)
     #blur = cv2.blur(mask,(5,5))
     edges = cv2.Canny(mask, 100,200)
-    cv2.imshow("edges",edges)
+    #cv2.imshow("edges",edges)
     try:
         # find the contours and keep the largest one
         #(_,cnts, _) = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #used to be thr
@@ -99,34 +101,67 @@ while rval:
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(src, 'cx: '+str(cx),(20,100), font ,1, (255, 255, 0), 2)
         cv2.putText(src, 'cy: '+str(cy),(20,70), font ,1, (255, 255, 0), 2)
-        cv2.circle(src, (cx,cy), 20, (255,105,180), -1)
+        cv2.circle(src, (cx,cy), 10, (255,255,255), -1)
 
         print ('cx is...', cx)
         print ('cy is...', cy)
-        print('hi')
 
 
-        '''DOESNT WORK '''
+
+
+        #DOESNT WORK
         #c = max(cnts,key = cv2.contourArea, default=1)   # draw bounding recangle
 
-        x,y,w,h = cv2.boundingRect(cnts)
+        x,y,w,h = cv2.boundingRect(cnt)
         cv2.rectangle(src,(x,y),(x+w,y+h),(128,255,0),2)
+
+        '''
+        rect = cv2.minAreaRect(cnt)
+        print rect
+        box = cv2.boxPoints(rect)
         print('boop')
-        #dislay var
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(src,'cogx: '+str(x +w/2),(10,50), font, .75,(0,255,0),2,cv2.LINE_AA)
-        cv2.putText(src,'cogy: '+str(y+h/2),(10,80), font, .75,(0,255,0),2,cv2.LINE_AA)
-        cv2.putText(src,'fps: '+str(camfps),(10,110), font, .75,(0,255,0),2,cv2.LINE_AA)
+        box = np.int0(box)
+        cv2.drawContours(src,[box],0,(0,255,255),2)
+        '''
 
 
         # send to network tables
         print('SendingX... ', (x+w/2))
-        #sd.putNumber('COG_X', (x+w/2))
+        sd.putNumber('COG_X', (x+w/2))
         print('SendingY... ', (y+h/2))
-        #sd.putNumber('COG_Y', (y+h/2))
+        sd.putNumber('COG_Y', (y+h/2))
 
+        '''
+        moments = cv.Moments(cv.fromarray(edges),i)
+        area = cv.GetCentralMoment(moments, 0, 0)
+
+        x = cv.GetSpatialMoment(moments, 1, 0)/area
+        y = cv.GetSpatialMoment(moments, 0, 1)/area
+
+        #create an overlay to mark the center of the tracked object
+        overlay = cv.CreateImage(cv.GetSize(cv.fromarray(src)), 8, 3)
+
+        cv.Circle(cv.fromarray(src), (int(x), int(y)), 2, (255, 255, 255), 20)
+        print ('area is...', int(area))
+        print ('x is...', int(x))
+        print ('y is...', int(y))
+        print ((x/320)*2)-1
+        cv2.imshow("src", src)
+        '''
+        hull = cv2.convexHull(cnt,returnPoints = False)
+        defects = cv2.convexityDefects(cnt,hull)
+
+        for j in range(defects.shape[0]):
+            s,e,f,d = defects[j,0]
+            start = tuple(cnt[s][0])
+            end = tuple(cnt[e][0])
+            far = tuple(cnt[f][0])
+            cv2.line(src,start,end,[0,255,0],2)
+        key = cv2.waitKey(20)
+        if key == 27:
+            break
     except:
-        print('oops')
+        print('an error has occured')
 
     # display image
     cv2.imshow("src", src)
